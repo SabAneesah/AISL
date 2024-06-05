ce670c06d5e65a6cecea4c9d8f856a5928420401
SabAneesah <aneesha13sabar@gmail.com>
2024-06-05 21:32:37 +0530

    File structure changed

--- src/FrontEnd/Pages/Dashboard.js
+++ src/FrontEnd/Pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useContext } from "react";
-import { DataContext } from "../routes/UserRoute";
+import { DataContext } from "../../routes/UserRoute";
 
 const Dashboard = () => {
   //const fullname = data[0].fullName;
--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -8,7 +8,7 @@ import {
 
   import { createContext,useState,useEffect } from "react";  
   
-  import Dashboard from "../Pages/Dashboard";
+  import Dashboard from "../FrontEnd/Dashboard";
   import FlashCardPage from "../Pages/FlashCardPage";
   import ViewFlashcard from "../Pages/ViewFlashcard";
 
